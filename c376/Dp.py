72d914e858b2305f111552d92b8ac50e3affb00f
Arnaldo Morais <arma@cin.ufpe.br>
2019-03-24 01:35:41 +0100

    C376 Done

--- c376/Dp.py
+++ c376/Dp.py
@@ -2,21 +2,34 @@
 PEP 484 over PEP3107
 '''
 
-def leaps(a,b):
-    boolean = False
-    if(b < a or b-a == 0):
-        return 0
-    if(a%4 == 0):
-        boolean = True
-    if(a%100 == 0):
-        boolean = False
-    if( (a%900) == (200 or 600) ):
-        boolean = True
-    #Like a ternary conditional operator
-    return (1 if boolean else 0)
+import math
 
+def multiplesBetweenRange(target,x,y):
+    return (math.floor(y*1.0/target) - math.ceil(x*1.0/target) + 1.0)
+
+    
+def leaps(a,b):
+    print (multiplesBetweenRange(4,a,b-1) - 
+    multiplesBetweenRange(100,a,b-1) + 
+    (multiplesBetweenRange(900,a-200,b-200-1) + multiplesBetweenRange(900,a-600,b-600-1)))
+
+    return (multiplesBetweenRange(4,a,b-1) - 
+    multiplesBetweenRange(100,a,b-1) + 
+    (multiplesBetweenRange(900,a-200,b-200-1) + multiplesBetweenRange(900,a-600,b-600-1)))
+    
 def main():
-    print 
+    # print num_between(1234,5678,900,200)
+    # print num_between(1234,5678,900,600)
+    # print check900Condition(1234,5678)
+    # print multiplesBetweenRange(900,1234,5678)
+    leaps(2016, 2017) 
+    leaps(2019, 2020) 
+    leaps(1900, 1901) 
+    leaps(2000, 2001) 
+    leaps(2800, 2801) 
+    leaps(123456, 123456) 
+    leaps(1234, 5678) 
+    leaps(123456, 7891011)
 
 if __name__ == "__main__":
     main()
